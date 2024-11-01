f1d9fe12e318d6038d8785aed31996ce790720c7
MagicSharp77 <149587362+MagicSharp77@users.noreply.github.com>
2024-11-01 16:52:23 +0800

    11

--- src/main/java/csu/mypetstoree/persistence/impl/ItemDaoImpl.java
+++ src/main/java/csu/mypetstoree/persistence/impl/ItemDaoImpl.java
@@ -1,36 +1,163 @@
 package csu.mypetstoree.persistence.impl;
 
 import csu.mypetstoree.domain.Item;
+import csu.mypetstoree.domain.Product;
+import csu.mypetstoree.persistence.DBUtil;
 import csu.mypetstoree.persistence.ItemDao;
 
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class ItemDaoImpl implements ItemDao {
 
-    public static final String UPDATE_INVENTORY_QUANTITY =
-        "SELECT I.ITEMID,LISTPRICE,UNITCOST,SUPPLIER AS supplierId,I.PRODUCTID AS product.productId,NAME AS product.name," +
-                "DESCN AS product.description,CATEGORY AS product.categoryId,STATUS," +
-                "ATTR1 AS attribute1,ATTR2 AS attribute2,ATTR3 AS attribute3,ATTR4 AS attribute4,ATTR5 AS attribute5" +
-                "FROM ITEM I, PRODUCT P WHERE P.PRODUCTID = I.PRODUCTID AND I.PRODUCTID = ?";
+    public static final String UPDATE_INVENTORY_QUANTITY ="UPDATE INVENTORY SET QTY = QTY - ? WHERE ITEMID = ?";
+
+    public static final String GET_INVENTORY_QUANTITY ="SELECT QTY AS QUANTITY FROM INVENTORY WHERE ITEMID = ?";
+
+    public static final String GET_ITEM =
+            "SELECT I.ITEMID,LISTPRICE,UNITCOST,SUPPLIER AS supplierId,I.PRODUCTID AS product.productId,NAME AS product.name," +
+                    "DESCN AS product.description,CATEGORY AS product.categoryId,STATUS," +
+                    "ATTR1 AS attribute1,ATTR2 AS attribute2,ATTR3 AS attribute3,ATTR4 AS attribute4,ATTR5 AS attribute5" +
+                    "FROM ITEM I, PRODUCT P WHERE P.PRODUCTID = I.PRODUCTID AND I.PRODUCTID = ?";;
+
+    public static final String GET_ITEMLIST_BY_PRODUCT =
+            "SELECT I.ITEMID,LISTPRICE,UNITCOST,SUPPLIER AS supplierId,I.PRODUCTID AS product.productId,NAME AS product.name," +
+            "DESCN AS product.description,CATEGORY AS product.categoryId,STATUS," +
+            "ATTR1 AS attribute1,ATTR2 AS attribute2,ATTR3 AS attribute3,ATTR4 AS attribute4,ATTR5 AS attribute5" +
+            "FROM ITEM I, PRODUCT P WHERE P.PRODUCTID = I.PRODUCTID AND I.PRODUCTID = ?";;
 
     @Override
     public void updateInventoryQuantity(Map<String, Object> param) {
-
+        try {
+            Connection conn = DBUtil.getConnection();
+            PreparedStatement ps = conn.
+                    prepareStatement(UPDATE_INVENTORY_QUANTITY);
+            String itemId = param.keySet().iterator().next();
+            Integer increment = (Integer) param.get(itemId);
+            ps.setInt(1, increment.intValue());
+            ps.setString(2, itemId);
+            ps.executeUpdate();
+            DBUtil.closePreparedStatement(ps);
+            DBUtil.closeConnection(conn);
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public int getInventoryQuantity(String itemId) {
-        return 0;
+        int result = -1;
+        try {
+            Connection connection = DBUtil.getConnection();
+            PreparedStatement pStatement = connection
+                    .prepareStatement(GET_INVENTORY_QUANTITY);
+            pStatement.setString(1, itemId);
+            ResultSet resultSet = pStatement.executeQuery();
+            if (resultSet.next()) {
+                result = resultSet.getInt(1);
+            }
+            DBUtil.closeResultSet(resultSet);
+            DBUtil.closePreparedStatement(pStatement);
+            DBUtil.closeConnection(connection);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     @Override
     public List<Item> getItemListByProduct(String productId) {
-        return List.of();
+        List<Item> itemList = new ArrayList<Item>();
+        try {
+            Connection connection = DBUtil.getConnection();
+            PreparedStatement ps = connection.prepareStatement(GET_ITEMLIST_BY_PRODUCT);
+            ps.setString(1, productId);
+            ResultSet resultSet = ps.executeQuery();
+            while (resultSet.next()) {
+//                Item item = new Item();
+//                item.setItemId(resultSet.getString(1));
+//                item.setProductId(resultSet.getString(2));
+//                item.setListPrice(resultSet.getBigDecimal(3));
+//
+//                item.setUnitCost(resultSet.getBigDecimal(4));
+//                item.setSupplierId(resultSet.getInt(5));
+//                item.setStatus(resultSet.getString(6));
+//                item.setAttribute1(resultSet.getString(7));
+//                item.setAttribute2(resultSet.getString(8));
+//                item.setAttribute3(resultSet.getString(9));
+//                item.setAttribute4(resultSet.getString(10));
+//                item.setAttribute5(resultSet.getString(11));
+//                itemList.add(item);
+                Item item = new Item();
+                item.setItemId(resultSet.getString(1));
+                item.setListPrice(resultSet.getBigDecimal(2));
+                item.setUnitCost(resultSet.getBigDecimal(3));
+                item.setSupplierId(resultSet.getInt(4));
+                Product product = new Product();
+                product.setProductId(resultSet.getString(5));
+                product.setName(resultSet.getString(6));
+                product.setDescription(resultSet.getString(7));
+                product.setCategoryId(resultSet.getString(8));
+                item.setProduct(product);
+                item.setStatus(resultSet.getString(9));
+                item.setAttribute1(resultSet.getString(10));
+                item.setAttribute2(resultSet.getString(11));
+                item.setAttribute3(resultSet.getString(12));
+                item.setAttribute4(resultSet.getString(13));
+                item.setAttribute5(resultSet.getString(14));
+                itemList.add(item);
+            }
+            DBUtil.closeResultSet(resultSet);
+            DBUtil.closePreparedStatement(ps);
+            DBUtil.closeConnection(connection);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return itemList;
     }
 
     @Override
     public Item getItem(String itemId) {
-        return null;
+        Item item = null;
+        try {
+            Connection connection = DBUtil.getConnection();
+            PreparedStatement pStatement = connection
+                    .prepareStatement(GET_ITEM);
+            pStatement.setString(1, itemId);
+            ResultSet resultSet = pStatement.executeQuery();
+            if (resultSet.next()) {
+                item = new Item();
+                item.setItemId(resultSet.getString(1));
+                item.setListPrice(resultSet.getBigDecimal(2));
+                item.setUnitCost(resultSet.getBigDecimal(3));
+                item.setSupplierId(resultSet.getInt(4));
+                Product product = new Product();
+                product.setProductId(resultSet.getString(5));
+                product.setName(resultSet.getString(6));
+                product.setDescription(resultSet.getString(7));
+                product.setCategoryId(resultSet.getString(8));
+                item.setProduct(product);
+                item.setStatus(resultSet.getString(9));
+                item.setAttribute1(resultSet.getString(10));
+                item.setAttribute2(resultSet.getString(11));
+                item.setAttribute3(resultSet.getString(12));
+                item.setAttribute4(resultSet.getString(13));
+                item.setAttribute5(resultSet.getString(14));
+                item.setQuantity(resultSet.getInt(15));
+            }
+            DBUtil.closeResultSet(resultSet);
+            DBUtil.closePreparedStatement(pStatement);
+            DBUtil.closeConnection(connection);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return item;
     }
 }
