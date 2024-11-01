70cb3727fa1290a3effd8f84ffcd8b52e53c87ca
2561651665 <149984263+2561651665@users.noreply.github.com>
2024-11-01 12:51:17 +0800

    cart

--- src/main/java/mypetstore/web/servlet/UpdateCartServlet.java
+++ src/main/java/mypetstore/web/servlet/UpdateCartServlet.java
@@ -3,11 +3,11 @@ package mypetstore.web.servlet;
 import mypetstore.domain.Cart;
 import mypetstore.domain.CartItem;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Iterator;
 
